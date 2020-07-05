e358e6e2bac801e269b4bc3765bc9fd254acaa13
hamadatakaki <i16hamadat@gmail.com>
2020-07-05 11:44:27 +0900

    Extend Alphabet input

--- Program.fs
+++ Program.fs
@@ -10,10 +10,10 @@ module OhTomaton =
         | S2
 
         static member AllState = [ 
-                yield S0
-                yield S1 
-                yield S2
-            ]
+            yield S0
+            yield S1 
+            yield S2
+        ]
 
         static member ToInt s =
             match s with
@@ -24,7 +24,6 @@ module OhTomaton =
         interface System.IEquatable<State> with
             member this.Equals(other) = State.ToInt this = State.ToInt other
 
-
     type Alphabet =
         | A
         | B
@@ -34,16 +33,30 @@ module OhTomaton =
             yield B
         ]
 
+        static member FromChar (c: char) =
+            match c with
+            | 'A' -> Some(A)
+            | 'B' -> Some(B)
+            | _ -> None
+
+        static member FromString (str: string) =
+            let pushAcc (acc: List<Alphabet>) (o: Option<Alphabet>) =
+                match o with
+                | Some(a) -> List.append acc [a]
+                | None -> acc
+            Seq.toList str
+            |> List.fold (fun acc c -> pushAcc acc (Alphabet.FromChar c)) []
+
     let transition q a =
         match (q, a) with
         | (State.S1, Alphabet.B) | (State.S2, Alphabet.A) -> State.S0
         | (State.S0, Alphabet.A) | (State.S2, Alphabet.B) -> State.S1
         | _ -> State.S2
 
-    type DFA<'Q, 'A when 'Q: equality>(q: List<'Q>, sigma: List<'A>, delta: 'Q -> 'A -> 'Q, q0: 'Q, f: List<'Q>) =
+    type DFA<'Q, 'A when 'Q: equality> (q: List<'Q>, sigma: List<'A>, delta: 'Q -> 'A -> 'Q, q0: 'Q, f: List<'Q>) =
         member this.ReadAlphabet (a: 'A) = delta q0 a
 
-        member this.ReadString (str: List<'A>) = 
+        member this.ReadAlphabetList (str: List<'A>) = 
             let rec applyDelta q str = 
                 match str with
                 | head :: tail -> applyDelta (delta q head) tail
@@ -51,7 +64,7 @@ module OhTomaton =
             applyDelta q0 str
 
         member this.IsAccept (str: List<'A>) =
-            List.contains (this.ReadString str) f
+            List.contains (this.ReadAlphabetList str) f
 
     let Q = State.AllState
     let Sigma = Alphabet.AllAlphabet
@@ -62,15 +75,17 @@ module OhTomaton =
     Automaton.ReadAlphabet Alphabet.A
     |> State.ToInt
     |> printfn "%d"
-    
-    Automaton.ReadString [Alphabet.A; Alphabet.A; Alphabet.B; Alphabet.A; Alphabet.A; Alphabet.A; Alphabet.A]
+
+    Alphabet.FromString "AABAAAA"
+    |> Automaton.ReadAlphabetList
     |> State.ToInt
     |> printfn "%d"
 
     // case true
-    Automaton.IsAccept [Alphabet.A; Alphabet.A; Alphabet.B; Alphabet.A; Alphabet.A; Alphabet.A; Alphabet.A]
+    Alphabet.FromString "AABAAAA"
+    |> Automaton.IsAccept
     |> printfn "%b"
 
-    // case false
-    Automaton.IsAccept [Alphabet.A; Alphabet.A; Alphabet.A; Alphabet.A]
+    Alphabet.FromString "AABAAAA"
+    |> Automaton.IsAccept
     |> printfn "%b"
